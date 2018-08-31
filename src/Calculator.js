445f5ecf7c291751275282e07e27cc181c916eaa
bibek <bibeksinghthapa@gmail.com>
2018-08-31 13:39:44 -0400

    subtraciton, multiplication and division added

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,17 +6,50 @@ class Calculator extends Component {
   constructor(props) {
     super(props)
 
-    this.add = this.add.bind(this)
+    this.plus = this.plus.bind(this)
+    this.minus = this.minus.bind(this)
+    this.multi = this.multi.bind(this)
+    this.divide = this.divide.bind(this)
+
     this.state = {
       sum:0
+    }
+    this.state = {
+      minus:0
+    }
+    this.state = {
+      multi:0
+    }
+    this.state = {
+      divide:0
     }
 
   }
 
-  add() {
+  plus() {
    var sum = (parseInt(this.refs.val1.value) || 0) + (parseInt(this.refs.val2.value) || 0);
    this.setState({
      sum: sum
+      })
+    }
+
+
+  minus() {
+   var minus = (parseInt(this.refs.val3.value) || 0) - (parseInt(this.refs.val4.value) || 0);
+   this.setState({
+     minus:minus
+      })
+    }
+  multi() {
+   var multi = (parseInt(this.refs.val5.value) || 0) * (parseInt(this.refs.val6.value) || 0);
+   this.setState({
+     multi:multi
+      })
+    }
+  divide() {
+   var divide = (parseInt(this.refs.val7.value) || 0) / (parseInt(this.refs.val8.value) || 0);
+   this.setState({
+     divide:divide
       })
     }
 
@@ -25,15 +58,43 @@ class Calculator extends Component {
   render(){
     return (
   <div className="container">
-      <h1>Add with React!</h1>
+      <h1>Math with React!</h1>
 
     <div className="add">
-    <input type="text" ref='val1' onKeyUp={this.add}/>
-    <span>+</span>
-    <input type="text" ref='val2' onKeyUp={this.add}/>
-    <span>=</span>
-    <h3>{this.state.sum}</h3>
+      <h3> Add </h3>
+      <input type="text" ref='val1' onKeyUp={this.plus}/>
+      <span>+</span>
+      <input type="text" ref='val2' onKeyUp={this.plus}/>
+      <span>=</span><span>{this.state.sum}</span>
+
     </div>
+
+    <div className="subtract">
+    <h3> Subtract </h3>
+      <input type="text" ref='val3' onKeyUp={this.minus}/>
+      <span>-</span>
+      <input type="text" ref='val4' onKeyUp={this.minus}/>
+      <span>=</span> <span>{this.state.minus}</span>
+    </div>
+
+    <div className="multiply">
+      <h3> Multiply</h3>
+      <input type="text" ref='val5' onKeyUp={this.multi}/>
+      <span>x</span>
+      <input type="text" ref='val6' onKeyUp={this.multi}/>
+      <span>=</span>  <span>{this.state.multi}</span>
+
+    </div>
+    <div className="divide">
+
+      <h3> divide</h3>
+      <input type="text" ref='val7' onKeyUp={this.divide}/>
+      <span>/</span>
+      <input type="text" ref='val8' onKeyUp={this.divide}/>
+      <span>=</span> <span>{this.state.divide}</span>
+
+    </div>
+
   </div>
 
     )
